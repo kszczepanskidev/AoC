0c2bf1d4570daeb5f6c2b974bac7a2bc90c4d9f3
Kamil Szczepański <kamil.szczepanski@x-kom.org>
2025-12-05 11:06:58 +0100

    [2025] Day 5

---
+++ 2025/05.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append("..")
+from helpers import fetchInput, merge_ranges
+from re import match
+
+input = fetchInput(__file__)
+
+split_index = input.index('')
+ranges = [(int(s), int(e)) for s, e in [r.split('-') for r in input[:split_index]]]
+products = [int(p) for p in input[split_index+1:]]
+
+def a(): #758
+    fresh_products = []
+
+    for product in products:
+        if product in fresh_products:
+            continue
+        for fresh_range in ranges:
+            if fresh_range[0] <= product <= fresh_range[1]:
+                fresh_products.append(product)
+                break
+
+    return len(fresh_products)
+
+def b(): #343143696885053
+    return sum([len(r) for r in [range(r[0],r[1]+1) for r in merge_ranges(ranges)]])
+
+
+print(f'a: {a()}')
+print(f'b: {b()}')
--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 from os import environ
 from re import match
 from requests import get
+from itertools import combinations
 
 
 def fetchInput(name):
@@ -29,3 +30,28 @@ def replace(string, character, index):
     if index not in range(len(string)):
         return string
     return string[:index] + character + string[index+1:]
+
+def merge_ranges(ranges):
+    def find_overlap(list):
+        for range1, range2 in combinations(list, 2):
+            min1, max1 = range1
+            min2, max2 = range2
+
+            if min1 > max2 or max1 < min2:
+                continue
+            else:
+                return range1, range2
+
+        return None
+
+    merged_list = ranges
+    while True:
+        overlap = find_overlap(merged_list)
+        if not overlap:
+            return merged_list
+        range1, range2 = overlap
+
+        merged_list.remove(range1)
+        merged_list.remove(range2)
+        range_values = range1 + range2
+        merged_list.append((min(range_values), max(range_values)))
